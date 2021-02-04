9f2f1636d0bb69ef550ec3328ed1cc6ec2970b5b
lmatarodo <lmatarodo@naver.com>
2021-02-04 13:54:23 +0900

    created report_card.c

---
+++ report_card.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+	int korean = 90;
+	int math = 80;
+	int english = 70;
+
+	printf("Korean : %d\n", korean);
+        printf("Math : %d\n", math);
+	printf("english : %d\n, english);
+
+	return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
