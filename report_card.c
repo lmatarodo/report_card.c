9355be56a1efe2f8b61f9210b6b2c0277dfd17ad
lmatarodo <lmatarodo@naver.com>
2021-02-04 14:07:09 +0900

    added science score

--- report_card.c
+++ report_card.c
@@ -4,10 +4,12 @@ int main() {
 	int korean = 90;
 	int math = 80;
 	int english = 70;
+	int science = 60;
 
 	printf("Korean : %d\n", korean);
         printf("Math : %d\n", math);
-	printf("english : %d\n, english);
+	printf("english : %d\n", english);
+	printf("Science : %d\n", science);
 
 	return 0;
 
